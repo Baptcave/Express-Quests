af5d322cab6af3211909d954308cb7fa7f529170
Baptcave <[noye.baptiste@orange.fr](mailto:noye.baptiste@orange.fr)>
2023-01-22 17:28:38 +0100

    finished the quest, let's move on to the challenge

--- movieHandlers.js
+++ movieHandlers.js
@@ -25,20 +25,36 @@ const movies = [
   },
 ];
 
+const database = require('./database');
+
 const getMovies = (req, res) => {
-  res.json(movies);
+  database
+    .query('SELECT * FROM movies')
+    .then(([movies]) => {
+        res.status(200).json(movies);
+    })
+    .catch((err) => {
+        console.error(err);
+        res.status(500).send('Could not retrieve data from database');
+    });
 };
 
 const getMovieById = (req, res) => {
   const id = parseInt(req.params.id);
 
-  const movie = movies.find((movie) => movie.id === id);
-
-  if (movie != null) {
-    res.json(movie);
-  } else {
-    res.status(404).send("Not Found");
-  }
+  database
+    .query('SELECT * FROM movies WHERE id = ?', [id])
+    .then(([movies]) => {
+        if (movies[0]) {
+          res.status(200).json(movies[0]);
+        } else {
+          res.status(404).send('Could not find movie')
+        }
+    })
+    .catch((err) => {
+        console.error(err);
+        res.status(500).send('Could not retrieve data from database');
+    });
 };
 
 module.exports = {
